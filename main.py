4e91d82289d1672ee42a7731dbbb7b1e8d3b0e4e
Nasrudin Bin Salim <Nasrudin.Salim.Suden@gmail.com>
2021-09-04 14:59:31 +0800

    Reformat code, added exception handling, some simple refactor for maintainance

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 import json
 import os
 import time
-import urllib
+import urllib.parse
 from ast import literal_eval
 
 import requests
@@ -11,7 +11,7 @@ import requests
 HACHI_SEARCH_PAGE_LINK = (
     "https://www.hachi.tech/game-on/gaming-consoles-accessories/playstation-consoles"
 )
-SEARCH_ITEM = "PS5"
+SEARCH_ITEM = "PS4"
 
 HACHI_JS_LINK = "https://www.hachi.tech/js/app.js"
 HACHI_PRODUCT_LINK = "https://www.hachi.tech/sony/sony/obffr/"
@@ -19,7 +19,9 @@ SEARCH_QUERY = "https://{ALGOLIA_APPLICATION_ID}-dsn.algolia.net/1/indexes/*/que
 
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
-TWILIO_WHATSAPP_FROM = os.environ.get("TWILIO_FROM")
+TWILIO_WHATSAPP_FROM = os.environ.get(
+    "TWILIO_FROM", "+14155238886"
+)  # Default Twilio Sandbox number
 TWILIO_WHATSAPP_TO = os.environ.get("TWILIO_TO")
 
 # Sandbox settings
@@ -46,36 +48,16 @@ found = False
 if ENABLED_TWILIO:
     from twilio.rest import Client
 
-    TWILIO_WHATSAPP_TO = TWILIO_WHATSAPP_TO.split(",")
+    TWILIO_WHATSAPP_TO = TWILIO_WHATSAPP_TO.replace(" ", "").strip().split(",")
+    print(f"Will be sending whatsapp messages to these people: {TWILIO_WHATSAPP_TO}")
 
     # Find your Account SID and Auth Token at twilio.com/console
     # and set the environment variables. See http://twil.io/secure
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 
-def main():
-    global found
-    print(f"We are searching for {SEARCH_ITEM} from {HACHI_SEARCH_PAGE_LINK}")
-
-    link_page = requests.get(HACHI_SEARCH_PAGE_LINK)
-    link_content = str(link_page.content)
-
-    # Get JS location of script that contains Algoliasite keys
-
-    js_location = link_content.find(HACHI_JS_LINK)
-
-    js_location_end = js_location
-    not_found = True
-    while not_found:
-        link_candidate = link_content[js_location:js_location_end]
-        if link_candidate.endswith('"'):
-            not_found = False
-            js_link_with_id = link_candidate[:-1]
-        js_location_end += 1
-
-    # Get algoliaSite keys from js content
-
-    js_content = str(requests.get(js_link_with_id).content)
+def hack_algolia_keys_from_js(hachi_tech_js_link):
+    js_content = str(requests.get(hachi_tech_js_link).content)
     candidate_end = js_content.find("algoliaActiveSite:{type:String}}")
     candidate = js_content[:candidate_end]
     candidate_start = candidate.rfind("querySelector")
@@ -90,39 +72,42 @@ def main():
             break
         algolia_keys_end += 1
 
-    # Hack the Algolia API KEY and application id
-
     algolia_application_id, algolia_api_key = literal_eval(candidate)
 
-    search_query = SEARCH_QUERY.format(
-        ALGOLIA_APPLICATION_ID=algolia_application_id.lower()
+    return algolia_application_id, algolia_api_key
+
+
+def get_js_file_path_containing_algolia_keys(search_page_contents: str):
+    js_location = search_page_contents.find(HACHI_JS_LINK)
+    js_location_end = js_location
+    while True:
+        link_candidate = search_page_contents[js_location:js_location_end]
+        if link_candidate.endswith('"'):
+            return link_candidate[:-1]
+        js_location_end += 1
+
+
+def get_search_category(search_page_contents: str):
+    candidate_start = search_page_contents.find("search-criteria=") + len(
+        "search-criteria="
     )
-
-    # Derive Search Criteria
-
-    candidate_start = link_content.find("search-criteria=") + len("search-criteria=")
     candidate_end = candidate_start + 1
 
-    not_found = True
-
-    while not_found:
-
-        search_criteria = link_content[candidate_start:candidate_end]
+    while True:
+        search_criteria = search_page_contents[candidate_start:candidate_end]
         candidate_end += 1
 
         if search_criteria.count('"') == 2:
-            not_found = False
+            search_criteria = literal_eval(search_criteria).split("/")
+            search_criteria = " > ".join(search_criteria)
+            return search_criteria
 
-    search_criteria = literal_eval(search_criteria)
-    search_criterias = search_criteria.split("/")
 
-    search_criteria = " > ".join(search_criterias)
-
-    # % Search for the product
-
+def search(search_category, algolia_app_id, algolia_api_key):
     search_query_search_params = SEARCH_QUERY_SEARCH_PARAMS.format(
-        search_criteria=urllib.parse.quote(search_criteria)
+        search_criteria=urllib.parse.quote(search_category)
     )
+    search_query = SEARCH_QUERY.format(ALGOLIA_APPLICATION_ID=algolia_app_id.lower())
 
     search_results = requests.post(
         search_query,
@@ -130,7 +115,7 @@ def main():
             "x-algolia-agent": "Algolia for JavaScript (4.10.5); Browser (lite); instantsearch.js (4.29.0); Vue"
             " (2.6.14); Vue InstantSearch (3.8.1); JS Helper (3.5.5)",
             "x-algolia-api-key": algolia_api_key,
-            "x-algolia-application-id": algolia_application_id,
+            "x-algolia-application-id": algolia_app_id,
         },
         json={
             "requests": [
@@ -145,68 +130,113 @@ def main():
     search_content = str(search_results.content)
     results = json.loads(literal_eval(search_content))["results"]
 
-    found = []
+    return results
 
-    for result in results:
-        for hit in result["hits"]:
-            item_desc = hit["item_desc"]
-            item_image = hit["image_url"].replace("_thumb", "")
-            item_id = hit["item_id"]
 
-            if SEARCH_ITEM.lower() in item_desc.lower():
-                # Build the link
-                item_description = item_desc.split(" ")
-                item_description = [
-                    "".join([i for i in item if i.isalnum()])
-                    for item in item_description
-                ]
-                path = "--".join(["-".join(item_description), item_id]).lower()
+def is_item_wanted(item):
+    item_desc = item["item_desc"]
+    return SEARCH_ITEM.lower() in item_desc.lower()
 
-                found.append(
-                    dict(
-                        item_description=item_desc,
-                        item_image=item_image,
-                        item_id=item_id,
-                        path=path,
-                        link=urllib.parse.urljoin(HACHI_PRODUCT_LINK, path),
-                    )
-                )
 
-    if not found:
+def build_item_link(item_description, item_id):
+    item_description = item_description.split(" ")
+    item_description = [
+        "".join([i for i in item if i.isalnum()]) for item in item_description
+    ]
+    path = "--".join(["-".join(item_description), item_id]).lower()
+
+    return path, urllib.parse.urljoin(HACHI_PRODUCT_LINK, path)
+
+
+def parse_item(item):
+    item_desc = item["item_desc"]
+    item_image = item["image_url"].replace("_thumb", "")
+    item_id = item["item_id"]
+    price = item["prices"]["REGULAR"]
+    price_member = item["prices"]["MEMBER"]
+
+    path, link = build_item_link(item_desc, item_id)
+    return dict(
+        item_description=item_desc,
+        item_image=item_image,
+        item_id=item_id,
+        path=path,
+        link=urllib.parse.urljoin(HACHI_PRODUCT_LINK, path),
+        price=price,
+        member_price=price_member,
+    )
+
+
+def parse_search_results(results):
+    return [
+        parse_item(item)
+        for result in results
+        for item in result["hits"]
+        if is_item_wanted(item)
+    ]
+
+
+def intro_message(found_items):
+    if not found_items:
         message = f"Your {SEARCH_ITEM} is not available"
-        print(message)
+
     else:
         message = f"""Congratulations, we found your item!
-Your {SEARCH_ITEM} is available in {HACHI_SEARCH_PAGE_LINK}
-here are the links: """
-        print(message)
+    Your {SEARCH_ITEM} is available in {HACHI_SEARCH_PAGE_LINK}
+    here are the links: """
+    return message
 
-    message_pack = [message]
 
-    for i, item in enumerate(found):
+def build_message_items(found_items):
+    message_items = []
+
+    for i, item in enumerate(found_items):
         message = f"""
-Item No: {i + 1}
-Description: {item['item_description']}
-Link: {item['link']}
-Searched from: {HACHI_SEARCH_PAGE_LINK}
-Image: {item['item_image']}
+    Item No: {i + 1}
+    Description: {item['item_description']}
+    Price: ${item['price']} (Members: ${item['member_price']})
+    Link: {item['link']}
+    Searched from: {HACHI_SEARCH_PAGE_LINK}
+    Image: {item['item_image']}
 
-    """
+        """
+        message_items.append(message)
+    return message_items
 
-        print(message)
-        message_pack.append(message)
 
-    if ENABLED_TWILIO and found:
-        message_pack.append("Will alert you again in 1 hour if its still there")
-        print("Sending Whatsapp messages")
+def main():
+    global found
+    print(f"We are searching for {SEARCH_ITEM} from {HACHI_SEARCH_PAGE_LINK}")
 
-        for to in TWILIO_WHATSAPP_TO:
-            twilio_message = client.messages.create(
-                from_=f"whatsapp:{TWILIO_WHATSAPP_FROM}",
-                body="\n".join(message_pack),
-                to=f"whatsapp:{to}",
-            )
-        print(twilio_message.sid)
+    link_page = requests.get(HACHI_SEARCH_PAGE_LINK)
+    link_content = str(link_page.content)
+
+    js_link_with_id = get_js_file_path_containing_algolia_keys(link_content)
+    search_category = get_search_category(link_content)
+    algolia_application_id, algolia_api_key = hack_algolia_keys_from_js(js_link_with_id)
+
+    results = search(search_category, algolia_application_id, algolia_api_key)
+
+    found = parse_search_results(results)
+
+    message_pack = [f"*WHERE IS MY {SEARCH_ITEM}*", intro_message(found)]
+    message_items = build_message_items(found)
+    message_pack.extend(message_items)
+    body = "\n".join(message_pack)
+    if found:
+        body += "\n Will alert you again in 1 hour if its still there"
+
+        if ENABLED_TWILIO:
+
+            print("Sending Whatsapp messages")
+            for to in TWILIO_WHATSAPP_TO:
+                twilio_message = client.messages.create(
+                    from_=f"whatsapp:{TWILIO_WHATSAPP_FROM}",
+                    body=body,
+                    to=f"whatsapp:{to}",
+                )
+                print(twilio_message.sid)
+    print(body)
 
 
 def remind_renew_sandbox():
@@ -220,6 +250,7 @@ def remind_renew_sandbox():
                 body=f"Reminder to rejoin sandbox via replying to this message with {sandbox_rejoin_message}",
                 to=f"whatsapp:{to}",
             )
+            print(twilio_message.sid)
         # Remind in another 72 hours, But user not receive anymore reminders nor messages if they did not renew
         sandbox_expiry = datetime.datetime.now() + datetime.timedelta(hours=72)
 
@@ -227,8 +258,12 @@ def remind_renew_sandbox():
 if __name__ == "__main__":
     while True:
         while not found:
-            main()
-            remind_renew_sandbox()
+            try:
+                main()
+                remind_renew_sandbox()
+            except Exception as e:
+                print(f"Some error occurred, {e}")
+                found = False
             time.sleep(1)  # We ping every 1 second until found
 
         # When found and user is alerted, we don't want to spam, so we set a longer timer of 1 hour
